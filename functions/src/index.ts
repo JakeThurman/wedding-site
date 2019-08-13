b01077820b8ceafc8636943a5742184a2ac6964a
Jake Thurman <jake.thurman@highgear.com>
2019-08-13 10:42:37 -0400

    Fix issues with email func

--- functions/src/index.ts
+++ functions/src/index.ts
@@ -12,7 +12,7 @@ type ExpectedGuest = {
     name: string
 }
 
-type Response = {
+type RawResponse = {
     can_attend: boolean
     meal: string
     name: string
@@ -20,8 +20,8 @@ type Response = {
     timestamp: string //ISO-8601
 }
 
-type User = { 
-    rsvps: Dict<Response>
+type RawUser = { 
+    rsvps: Dict<RawResponse>
     guestInfo: {
         maxGuests: number
         name: string
@@ -41,8 +41,11 @@ type CleanedResponse = {
 
 type CleanedUser = {
     rsvps: CleanedResponse[]
+    uid: string
     newestResponse: Date
     label: string
+    guestListName: string
+    emailsSent: number
 };
 
 // Initialize Admin SDK
@@ -57,41 +60,40 @@ const mailTransport = nodemailer.createTransport(postmarkTransport({
     auth: { apiKey: postMarkConfig.key }
 }))
 
-// Each day, send an email
-
-export const sendTooManyEmails = functions.database.ref("users").onWrite(async () => {
-//export const dailyEmail = functions.pubsub.schedule("every 1 day").onRun(async () => {
-    const usersRef = admin.database().ref("users");
-    
-    // Grab the value of the users
-    const snap = await usersRef.once("value");
-    
-    const allUsers: Dict<User> = snap.val()
+export const sendTooManyEmails = functions.database.ref("users").onWrite(async (change) => {
+//export const dailyEmail = functions.pubsub.schedule("every 1 day").onRun(async () => {    
+    const allUsers = await getAllUsers();
 
     // Find all of the users with new responses
-    const newResponses = Object.entries(allUsers)
-                               .filter(pair => pair[1].emailsSent !== Object.keys(pair[1].rsvps).length);
+    const newResponses = allUsers.filter(user => user.emailsSent !== user.rsvps.length);
 
     if (!newResponses.length)
         return;
 
-    // Collect the data for an email
-    const guestsStillWaitingOn = await getRemainingGuests(Object.values(allUsers));
+    // Collect the list of guests we haven't heard back from.
+    const guestsStillWaitingOn = await getRemainingGuests(allUsers);
 
-    sendEmail(newResponses.map(([_, user]) => user), guestsStillWaitingOn);
+    sendEmail(newResponses, guestsStillWaitingOn);
 
     // Mark the responses we've already sent emails for.
-    const updates = toDict(newResponses.map(([uid, user]) => [`${uid}/emailsSent`, Object.keys(user.rsvps).length]));
-    await usersRef.update(updates);
+    const updates = toDict(newResponses.map(user => [`${user.uid}/emailsSent`, user.rsvps.length]));
+    await admin.database().ref("users").update(updates);
 });
 
-async function getRemainingGuests(existingUsers:User[]): Promise<ExpectedGuest[]> {
+async function getAllUsers(): Promise<CleanedUser[]> {
+    const usersRef = admin.database().ref("users");
+    const userSnapshot = await usersRef.once("value");
+
+    return sanitizeAndSortUsers(userSnapshot.val());
+}
+
+async function getRemainingGuests(existingUsers:CleanedUser[]): Promise<ExpectedGuest[]> {
     const guests: ExpectedGuest[] = await admin.database()
                                                .ref("guestlist")
                                                .once("value")
                                                .then(snap => snap.val());
 
-    const responseNames = new Set(existingUsers.map(u => u.guestInfo.name));
+    const responseNames = new Set(existingUsers.map(u => u.guestListName));
 
     return guests.filter(g => !responseNames.has(g.name));
 }
@@ -102,8 +104,8 @@ function toDict<T>(pairs: [string, T][]) {
     return result
 }
 
-function sanitizeAndSortUsers(users: User[]): CleanedUser[] {
-    return users.map((person) => {
+function sanitizeAndSortUsers(users: Dict<RawUser>): CleanedUser[] {
+    return Object.entries(users).map(([uid, person]) => {
         const rsvps = Object.entries(person.rsvps || {})
             .map(([rid, r]) => ({
                 rid: rid,
@@ -131,20 +133,21 @@ function sanitizeAndSortUsers(users: User[]): CleanedUser[] {
         const label = nameIsCorrect ? guestInfo.name : (guestInfo.name + " (typed: '" + guestInfo.enteredName + "')");
 
         return {
+            uid: uid,
             rsvps: rsvps,
             newestResponse: newestResponse,
             label: label,
+            emailsSent: person.emailsSent || 0,
+            guestListName: guestInfo.name,
         }
     }).sort(function (a, b) {
         return b.newestResponse.valueOf() - a.newestResponse.valueOf()
     });
 }
 
-function sendEmail(newUsers: User[], guestsStillWaitingOn: ExpectedGuest[]) {
+function sendEmail(newUsers: CleanedUser[], guestsStillWaitingOn: ExpectedGuest[]) {
     const stillWaitingContent = guestsStillWaitingOn.map(g => `${g.name} (${g.maxGuests})`)
                                                     .map(txt => `<li>${htmlEncode(txt)}</li>`)
-
-    const sanitizedUsers = sanitizeAndSortUsers(newUsers)
 
     const getRsvpItem = (r: CleanedResponse) => `
         <li>
@@ -153,7 +156,7 @@ function sendEmail(newUsers: User[], guestsStillWaitingOn: ExpectedGuest[]) {
             <div><b>Note</b>: <span style='white-space:pre-wrap'>${htmlEncode(r.note)}</span></div>
         </li>`
 
-    const newResponseItems = sanitizedUsers.map(u => `
+    const newResponseItems = newUsers.map(u => `
         <li>
             <h2>${htmlEncode(u.label)}</h2>
             <ul>${u.rsvps.map(getRsvpItem)}</ul>
@@ -162,7 +165,7 @@ function sendEmail(newUsers: User[], guestsStillWaitingOn: ExpectedGuest[]) {
     const mailOptions = {
         from: '"melissaandjake.com" <noreply@melissaandjake.com>',
         to: `jacob@thurmans.com`,
-        subject: `${sanitizedUsers.length} New Response${sanitizedUsers.length > 1 ? "s" : ""}`,
+        subject: `${newUsers.length} New Response${newUsers.length > 1 ? "s" : ""}`,
         html: `
             <html>
                 <body>
